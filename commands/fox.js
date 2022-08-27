60abfd23bfb05e5c256bc4aec2409a6f70e85605
wrembish <wrembish@udel.edu>
2022-08-27 08:59:18 -0400

    Added fox command

---
+++ commands/fox.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
+const { EMBEDCOLOR } = require('../emojibot_files/constants.js')
+
+module.exports = {
+    data : new SlashCommandBuilder()
+        .setName('fox')
+        .setDescription('Get a random fox picture'),
+    async execute(interaction) {
+        let result
+        await fetch('https://randomfox.ca/floof/')
+            .then(response => response.json())
+            .then(data => result = data)
+            .catch(error => console.error('Error: ', error))
+
+        const replyEmbed = new EmbedBuilder()
+            .setTitle('https://randomfox.ca/')
+            .setDescription('Random fox on every click!')
+            .setImage(result.image)
+            .setColor(EMBEDCOLOR)
+
+        await interaction.reply({ embeds : [replyEmbed] })
+    }
+}
